509e20c89f45214305192f9e26d1e988200ee4fb
Carbuino <19510288+Carbuino@users.noreply.github.com>
2024-04-20 13:00:57 -0600

    Update copyright and change wordings

--- core/src/main/java/org/geysermc/geyser/translator/protocol/bedrock/entity/player/BedrockEmoteTranslator.java
+++ core/src/main/java/org/geysermc/geyser/translator/protocol/bedrock/entity/player/BedrockEmoteTranslator.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2019-2023 GeyserMC. http://geysermc.org
+ * Copyright (c) 2019-2024 GeyserMC. http://geysermc.org
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
@@ -41,9 +41,8 @@ public class BedrockEmoteTranslator extends PacketTranslator<EmotePacket> {
     @Override
     public void translate(GeyserSession session, EmotePacket packet) {
         if (session.getGeyser().getConfig().getEmoteOffhandWorkaround() == EmoteOffhandWorkaroundOption.MENU) {
-            // Build the Menu
+            // Build and Send the Emote Menu
             session.sendForm(EmoteMenuUtils.buildEmoteForm(session, packet));            
-
             return;
         }
 
